c787d3b53058a2302bc63d39bd04a6eee620c7b2
jonathan-cj <13519144@std.stei.itb.ac.id>
2021-09-28 18:35:40 +0700

    local search done

--- src/ai/local_search.py
+++ src/ai/local_search.py
@@ -4,51 +4,71 @@ from time import time
 
 from src.constant import ShapeConstant
 from src.model import State
-
+from src.utility import place
+from src.ai.objective import countObjective
 from typing import Tuple, List
 
-from objective import getTopRow
-
+# from src.ai.objective import getTopRow
 
 
 class LocalSearch:
     def __init__(self):
         pass
 
-    #ngitung heuristik untuk tiap pilihan trus ambil aja
+    # ngitung heuristik untuk tiap pilihan trus ambil aja
     def find(self, state: State, n_player: int, thinking_time: float) -> Tuple[str, str]:
         self.thinking_time = time() + thinking_time
-        
-        best_movement = self.hillclimbing(state)
+
+        best_movement = self.hillclimbing(state, n_player)
 
         return best_movement
 
-    def hillclimbing(self, state: State):
-        #for every state, count objective, get max or min value from state, return that state
-        listofmoves = [[0,ShapeConstant.CROSS], [0,ShapeConstant.CIRCLE],
-        [1,ShapeConstant.CROSS], [1,ShapeConstant.CIRCLE],
-        [2,ShapeConstant.CROSS], [2,ShapeConstant.CIRCLE],
-        [3,ShapeConstant.CROSS], [3,ShapeConstant.CIRCLE],
-        [4,ShapeConstant.CROSS], [4,ShapeConstant.CIRCLE],
-        [5,ShapeConstant.CROSS], [5,ShapeConstant.CIRCLE],
-        [6,ShapeConstant.CROSS], [6,ShapeConstant.CIRCLE],
-        ]
+    def hillclimbing(self, state: State, n_player: int):
+        # for every state, count objective, get max or min value from state, return that state
+        # cek kuota
+        playerquota = self.state.players[n_player].quota
+        circle = playerquota[ShapeConstant.CIRCLE]
+        cross = playerquota[ShapeConstant.CROSS]
+        # isi semua kemungkinan move
+        listofmoves = []
+        for i in range(state.board.col):
+            if cross > 0:
+                listofmoves.append([i, ShapeConstant.CROSS])
+            if circle > 0:
+                listofmoves.append([i, ShapeConstant.CIRCLE])
+        # print(listofmoves)
         listofstates = {}
         for i in range(len(listofmoves)):
-            temp = copy.deepcopy(state) #copy to temp state
-            row = getTopRow(temp, listofmoves[i][0])
-            if row != -1: #jika tidak udh full rownya
-                temp.board.set_piece(row, listofmoves[i][0], listofmoves[i][1])
-                heuristic = 0 #ini ganti dapet nilai heuristic brp
+            temp = copy.deepcopy(state)  # copy to temp state
+
+            valid = place(temp, n_player, listofmoves[i][1], listofmoves[i][0])
+            if valid != -1:
+                heuristic = countObjective(temp)  # ini ganti dapet nilai heuristic brp
                 listofstates[i] = heuristic
-        
-        ekstrim = 0 #ini ganti max atau min
-        ans = [random.randint(0, 7), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE])]
-        for i in listofstates:
-            value = listofstates[i]
-            if (value > ekstrim): #ini ganti lebih besar atau lebih kecil
-                ekstrim = value
-                ans = listofmoves[i]
 
+        # print(listofstates)
+        ronde = state.round
+        if ronde % 2 == 1:
+            maks = True
+        else:
+            maks = False
+
+        ekstrim = listofstates[0]  # ini ganti max atau min
+        ans = [
+            random.randint(0, 7),
+            random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE]),
+        ]
+        if maks:
+            for i in listofstates:
+                value = listofstates[i]
+                if value > ekstrim:
+                    ekstrim = value
+                    ans = listofmoves[i]
+        else:
+            for i in listofstates:
+                value = listofstates[i]
+                if value < ekstrim:
+                    ekstrim = value
+                    ans = listofmoves[i]
+        # print("ans",ans)
         return ans
-
