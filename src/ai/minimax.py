8c7532e2ad63ae9d4444e058e358f984e1c406dc
billyjuliux <billyjulius01@gmail.com>
2021-10-19 11:56:43 +0700

    add timer for minimax

--- src/ai/minimax.py
+++ src/ai/minimax.py
@@ -1,6 +1,6 @@
 import random
+import multiprocessing as mp
 from time import time
-from multiprocessing import Process, Manager
 
 from src.constant import ShapeConstant, GameConstant
 from src.utility import place, check_streak
@@ -16,106 +16,85 @@ WIN_SCORE = 10000
 LOSE_SCORE = -10000
 
 class Minimax:
-    def __init__(self, max_depth = 5, startTime = 0):
+    def __init__(self, max_depth = 4):
         self.max_depth: int = max_depth
 
     def minimaxAlphaBeta(
-        self, node: Node, depth: int, isMaximizing: bool, alpha: int, beta: int, currTime: float
+        self, node: Node, depth: int, isMaximizing: bool, alpha: int, beta: int
     ):
-
         if not node :
             return None
 
-        #Kalo ketemu daun
-        if node.depth == self.max_depth: #BASIS
-            return countObjective(node.state, True) # musuh
+        # Basis
+        if node.depth == self.max_depth:
+            return countObjective(node.state, True)
 
-        #REKURENS
+        # Recurence
         if isMaximizing:
             maxVal = LOSE_SCORE
-
             for child in node.children:
-                if child : # check if not null
-                    currVal = self.minimaxAlphaBeta(child, depth + 1, False, alpha, beta, time())
+                if child :
+                    currVal = self.minimaxAlphaBeta(child, depth + 1, False, alpha, beta)
                     if (abs(child.value) != 10000):
                         child.value = currVal 
                     maxVal = max(maxVal, child.value)
                     alpha = max(alpha, maxVal)
                     if alpha >= beta:
-                        # maxVal = WIN_SCORE
                         break
-                    # if (currTime - self.startTime > 0) :
-                    #     print("too long")
-                    #     break
             return maxVal
+
         else:
             minVal = WIN_SCORE
-
             for child in node.children:
                 if child : 
-                    currVal = self.minimaxAlphaBeta(child, depth + 1, True, alpha, beta, time())
+                    currVal = self.minimaxAlphaBeta(child, depth + 1, True, alpha, beta)
                     if (abs(child.value) != 10000):
                         child.value = currVal
                     minVal = min(minVal, child.value)
                     beta = min(beta, minVal)
                     if alpha >= beta:
-                        # minVal = LOSE_SCORE
                         break
-                    # if (currTime - self.startTime > 0) :
-                    #     print("too long")
-                    #     break
             return minVal
+
+    def initRoot(self, root, return_dict):
+        createTree(root, self.max_depth)
+        self.minimaxAlphaBeta(root, 1, True, LOSE_SCORE, WIN_SCORE)
+        return_dict["root"] = root
+
 
     def find(
         self, state: State, n_player: int, thinking_time: float
     ) -> Tuple[str, str]:
-        # self.thinking_time = time() + thinking_time
-        # best_movement = (random.randint(0, state.board.col), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE])) #minimax algorithm
-        # return best_movement
 
-        # print(state.board)
-
-        # Make tree
-        # return_dict = Manager.dict()
-        self.startTime = time()
         root = Node(1, state, -999)
-        createTree(root, self.max_depth)
-        self.minimaxAlphaBeta(root, 1, True, LOSE_SCORE, WIN_SCORE, self.startTime)
+        manager = mp.Manager()
+        return_dict = manager.dict()
 
-        # p = Process(target=Minimax.minimaxAlphaBeta, args = (self, root, 1, True, LOSE_SCORE, WIN_SCORE))
-        # root.printTree()
-        # p.start()
-        # print("started p")
-        # print(thinking_time)
-        # p.join(thinking_time)
-        # print("joined p")
+        p = mp.Process(target=self.initRoot, args = (root, return_dict))
+        p.start()
+        p.join(thinking_time)
 
-        # if p.is_alive() :
-        #     print("too long, killing..")
-        #     p.terminate()
-        #     return random.randint(0, state.board.col)
-        # else :
+        if p.is_alive() :
+            print("Minimax algorithm is taking too long.. returning random action")
+            p.kill()
+            p.join()
+            return (random.randint(0, state.board.col), random.choice([ShapeConstant.CROSS, ShapeConstant.CIRCLE]))
+            
+        root = return_dict["root"]
         childStateIdxToMove = 0
         bestObjVal = -999
-        print("obj value depth 2 : ", end="")
+        # print("obj value depth 2 : ", end="")
         for i in range(state.board.col*2):
-            if root.children[i]:
-                print(str(i) + ":" + str(root.children[i].value) + " | ", end="")
+            # if root.children[i]:
+            #     print(str(i) + ":" + str(root.children[i].value) + " | ", end="")
             if root.children[i] and (root.children[i].value > bestObjVal): 
                 bestObjVal = root.children[i].value
                 childStateIdxToMove = i
-        print()
+        # print()
 
-        # print("chosen state : " + str(childStateIdxToMove))
         colToMove = childStateIdxToMove // 2
         if childStateIdxToMove % 2 == 0:
             shapeToMove = ShapeConstant.CROSS
         else:
             shapeToMove = ShapeConstant.CIRCLE
-        
-        # if (state.round == 6) :
-        #     print("print tree :")
-        #     root.printTree()
-
-        # root.printTree()
         return (colToMove, shapeToMove)
